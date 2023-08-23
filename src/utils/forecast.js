dec3c665014a94c5af402b42f4a2f485e9935968
Soubhik <gsoubhik01@gmail.com>
2023-08-23 18:51:11 +0530

    Added Humidity Feature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast =(latitude,longitude,callback) =>{
             callback('Unable to find location',undefined);
         } else {
             callback(undefined,
-                'It is currently '+body.current.temperature+' degrees. It feels like '+body.current.feelslike+' degrees out.'
+                'It is currently '+body.current.temperature+' degrees. It feels like '+body.current.feelslike+' degrees out. The humidity is '+ body.current.humidity+'%.'
             )
         }
     })
